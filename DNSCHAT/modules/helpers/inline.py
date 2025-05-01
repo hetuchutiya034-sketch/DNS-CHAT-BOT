9ca9de0c564bebfe7f5c2f649a8a27c645fb9859
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-05-01 11:16:22 +0530

    Update inline.py

--- DNSCHAT/modules/helpers/inline.py
+++ DNSCHAT/modules/helpers/inline.py
@@ -132,7 +132,7 @@ HELP_BUTN = [
 
 ABOUT_BTN = [
     [
-        InlineKeyboardButton(text="🎄 sᴜᴘᴘᴏʀᴛ 🎄", url=f"https://t.me/DNS_NETWORK"),
+        InlineKeyboardButton(text="🎄 sᴜᴘᴘᴏʀᴛ 🎄", url=f"https://t.me/RU_DRA_098"),
         InlineKeyboardButton(text="« ʜᴇʟᴘ »", callback_data="HELP"),
     ],
     [
@@ -140,7 +140,7 @@ ABOUT_BTN = [
         #   InlineKeyboardButton(text="❄️ sᴏᴜʀᴄᴇ ❄️", callback_data="SOURCE"),
     ],
     [
-        InlineKeyboardButton(text="🐳 ᴜᴘᴅᴀᴛᴇs 🐳", url=f"https://t.me/Dns_Official_Channel"),
+        InlineKeyboardButton(text="🐳 ᴜᴘᴅᴀᴛᴇs 🐳", url=f"https://t.me/RU_DRA_098"),
         InlineKeyboardButton(text="⦿ ʙᴀᴄᴋ ⦿", callback_data="BACK"),
     ],
 ]
