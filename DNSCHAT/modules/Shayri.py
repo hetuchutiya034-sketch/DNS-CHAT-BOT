b9fa9eb09a3164d2eb79f1c29ee601facea2ddcc
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-05-01 11:26:24 +0530

    Update Shayri.py

--- DNSCHAT/modules/Shayri.py
+++ DNSCHAT/modules/Shayri.py
@@ -75,10 +75,10 @@ async def shayri(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "• sᴜᴘᴘᴏꝛᴛ •", url=f"https://t.me/DNS_NETWORK"
+                        "• sᴜᴘᴘᴏꝛᴛ •", url=f"https://t.me/RU_DRA_098"
                     ),
                     InlineKeyboardButton(
-                        "• ᴜᴘᴅᴧᴛᴇ •", url=f"https://t.me/Dns_Official_Channel"
+                        "• ᴜᴘᴅᴧᴛᴇ •", url=f"https://t.me/RU_DRA_098"
                     ),
                 ]
             ]
