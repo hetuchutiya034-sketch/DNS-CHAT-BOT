f413cb26462908e8679cbe6ce43b4eb2092ba987
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2026-08-09 08:26:34 +0530

    Update config.py to load API credentials from env
    Refactor configuration to use environment variables for sensitive data.

--- config.py
+++ config.py
@@ -4,13 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "21552265"
-# -------------------------------------------------------------
-API_HASH = "1c971ae7e62cc416ca977e040e700d09"
-# --------------------------------------------------------------
+API_ID = int(getenv("API_ID", "0"))
+API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7408008545"))
-SUPPORT_GRP = "RU_DRA_098"
-UPDATE_CHNL = "RU_DRA_098"
-OWNER_USERNAME = "RU_DRA_65"
+OWNER_ID = int(getenv("OWNER_ID", "7302427268"))
+SUPPORT_GRP = getenv("SUPPORT_GRP", "")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "")
